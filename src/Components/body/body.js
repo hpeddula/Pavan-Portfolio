6677148f118cf732b6eb7c65c3a7db0de557efa7
Harsha <hpeddula@outlook.com>
2019-02-27 21:52:40 +0530

    Adding scss for body

--- src/Components/body/body.js
+++ src/Components/body/body.js
@@ -2,32 +2,45 @@ import React, { Component } from 'react'
 import Game1 from '../games/game1';
 import Game2 from '../games/game2';
 import { Switch, Route } from 'react-router-dom';
+import './body.scss';
 export default class Body extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            games: [{ id: 1, name: 'Game 1' },
-            { id: 2, name: 'Game 2' },
-            { id: 3, name: 'Game 3' },
-            { id: 4, name: 'Game 4' },
-            { id: 5, name: 'Game 5' }]
+            games: [{ id: 1, name: Game1 }]
         }
     }
     render() {
-        const { games } = this.state;
         return (
-            <div className="game-wrapper">
-                {
-                    games.map((game, index) => (
-                        <div class="card" style={{width:'18rem'}}>
-                            <div class="card-body">
-                                <h5 class="card-title" style={{backgroundColor:'black',color:'white'}}>Card title</h5>
-                                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
+            <React.Fragment>
+                <div className="game-wrapper">
+                    <div className="card">
+                        <div className="card-body">
+                            <p className="card-text">This is some text within a card body.</p>
                         </div>
-                    ))
-                }
-            </div >)
+                    </div>
+                    <div className="card">
+                        <div className="card-body">
+                            <p className="card-text">This is some text within a card body.</p>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="card-body">
+                            <p className="card-text">This is some text within a card body.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card-body">
+                        <p className="card-text">This is some text within a card body.</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card-body">
+                        <p className="card-text">This is some text within a card body.</p>
+                    </div>
+                </div>
+            </React.Fragment>
+        )
     }
 }
--- src/Components/header/header.js
+++ src/Components/header/header.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import './header.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGamepad } from '@fortawesome/free-solid-svg-icons';
+import { faGamepad, faKeyboard } from '@fortawesome/free-solid-svg-icons';
 export default function Header() {
     return (
         <React.Fragment>
+            <div className="text-center">
+                <h1 className="name">Pavan Tummala</h1>
+                <FontAwesomeIcon icon={faGamepad} color="white" size="2x" className="gamePad"/>
+                <FontAwesomeIcon icon={faKeyboard} color="white" size="2x" />
+                <p><code>Game Programming Portfolio</code></p>
+            </div>
             <div className="video-container">
                 <div className="video">
                     <iframe width="560" height="349" title="Introductory Video" src="https://www.youtube.com/embed/W3DBpti5giI?rel=0&modestbranding=1"></iframe>
                 </div>
-            </div>
-            <div className="text-center">
-                <h1 className="name">Pavan Tummala</h1>
-                <FontAwesomeIcon icon={faGamepad} color="white" size="2x" />
-                <p><code>Game Programming Portfolio</code></p>
             </div>
         </React.Fragment>
     )
