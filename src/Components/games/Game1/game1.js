cc35684538c265cf37e54c299a3b06f0ce21c8ea
hpeddula <hpeddula@outlook.com>
2019-03-11 11:03:10 +0530

    Wrapping text around image

--- src/Components/games/Game1/game1.js
+++ src/Components/games/Game1/game1.js
@@ -14,7 +14,13 @@ export default class Game1 extends Component {
         </nav>
         <div className="aaa">
           <div className="bbb">
-            <h1>This is game1</h1>
+          <img className="game-image" alt="Game1" src={require('../../../Assets/images/image.jpeg')} align="right" />
+            <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+            It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
           </div>
         </div>
       </div>
