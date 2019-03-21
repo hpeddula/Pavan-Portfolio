ef75c57d40150d6cd0739ac1e21520d4e2eb2d0f
Harsha <hpeddula@outlook.com>
2019-03-21 08:15:58 +0530

    adding portfolio api

---
+++ API/ContactAPI/ContactAPI/Models/ContactContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+namespace ContactAPI.Models
+{
+    public class ContactContext : DbContext
+    {
+        public ContactContext(DbContextOptions<ContactContext> options) : base(options)
+        {
+        }
+        public DbSet<ContactForm> Contacts { get; set; }
+
+    }
+}
---
+++ API/ContactAPI/ContactAPI/Models/ContactForm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ContactAPI.Models
+{
+    public class ContactForm
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Message { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
