5624737ee5f1f340b487858fdd6e4cf4c801af71
Harsha <hpeddula@outlook.com>
2019-03-21 19:36:11 +0530

    Updates

---
+++ precache-manifest.8574475239cd4a1c0ca577e023913eea.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "110f2b3ee518f7309100",
+    "url": "/Pavan-Portfolio/static/css/9.3b590829.chunk.css"
+  },
+  {
+    "revision": "56259021208b84e120fd",
+    "url": "/Pavan-Portfolio/static/css/main.336e0714.chunk.css"
+  },
+  {
+    "revision": "f7b13964696e9d563623",
+    "url": "/Pavan-Portfolio/static/js/runtime~main.f7b13964.js"
+  },
+  {
+    "revision": "7b7fcc19ce6b76b613290bd4287ce557",
+    "url": "/Pavan-Portfolio/static/media/5a4f67e45bafe35fa63db642.7b7fcc19.jpeg"
+  },
+  {
+    "revision": "1d6988683a7baf4822bd",
+    "url": "/Pavan-Portfolio/static/js/2.1d698868.chunk.js"
+  },
+  {
+    "revision": "f900a9892328f91f4f5a",
+    "url": "/Pavan-Portfolio/static/js/3.f900a989.chunk.js"
+  },
+  {
+    "revision": "c4681ce35873478d63d86c678417c5e9",
+    "url": "/Pavan-Portfolio/static/media/pgcqPXG.c4681ce3.jpg"
+  },
+  {
+    "revision": "5feaa7e9f38960ff8667",
+    "url": "/Pavan-Portfolio/static/js/4.5feaa7e9.chunk.js"
+  },
+  {
+    "revision": "fb8bbdfc8037a929078a",
+    "url": "/Pavan-Portfolio/static/js/5.fb8bbdfc.chunk.js"
+  },
+  {
+    "revision": "6b9d513bdaeaad1c350ef2de26711ca9",
+    "url": "/Pavan-Portfolio/static/media/pubg.6b9d513b.jpeg"
+  },
+  {
+    "revision": "0377818446a74d7913a9",
+    "url": "/Pavan-Portfolio/static/js/6.03778184.chunk.js"
+  },
+  {
+    "revision": "658c6e6316a8140f0f5d5ee8b555a530",
+    "url": "/Pavan-Portfolio/static/media/witcher3_en_wallpaper_the_witcher_3_wild_hunt_geralt_with_trophies_2560x1600_1449484679.658c6e63.png"
+  },
+  {
+    "revision": "05110a9f03ac22fa716e",
+    "url": "/Pavan-Portfolio/static/js/7.05110a9f.chunk.js"
+  },
+  {
+    "revision": "d5499fe609190f2bd549",
+    "url": "/Pavan-Portfolio/static/js/8.d5499fe6.chunk.js"
+  },
+  {
+    "revision": "56259021208b84e120fd",
+    "url": "/Pavan-Portfolio/static/js/main.56259021.chunk.js"
+  },
+  {
+    "revision": "110f2b3ee518f7309100",
+    "url": "/Pavan-Portfolio/static/js/9.110f2b3e.chunk.js"
+  },
+  {
+    "revision": "69c7dbe8db7627ea8e19",
+    "url": "/Pavan-Portfolio/static/js/10.69c7dbe8.chunk.js"
+  },
+  {
+    "revision": "2f1a2117a2b3ef05429b",
+    "url": "/Pavan-Portfolio/static/js/11.2f1a2117.chunk.js"
+  },
+  {
+    "revision": "73c9b88ce8e0b5a0344d",
+    "url": "/Pavan-Portfolio/static/js/12.73c9b88c.chunk.js"
+  },
+  {
+    "revision": "a13f4fbeddff29eaf868",
+    "url": "/Pavan-Portfolio/static/js/13.a13f4fbe.chunk.js"
+  },
+  {
+    "revision": "654592419f3f7133b454",
+    "url": "/Pavan-Portfolio/static/js/14.65459241.chunk.js"
+  },
+  {
+    "revision": "f3b0146a5dae89ccaceb",
+    "url": "/Pavan-Portfolio/static/js/15.f3b0146a.chunk.js"
+  },
+  {
+    "revision": "eb0ab4c617dd943b9eff",
+    "url": "/Pavan-Portfolio/static/js/16.eb0ab4c6.chunk.js"
+  },
+  {
+    "revision": "05110a9f03ac22fa716e",
+    "url": "/Pavan-Portfolio/static/css/7.adf1f078.chunk.css"
+  },
+  {
+    "revision": "0377818446a74d7913a9",
+    "url": "/Pavan-Portfolio/static/css/6.39fc66ff.chunk.css"
+  },
+  {
+    "revision": "5feaa7e9f38960ff8667",
+    "url": "/Pavan-Portfolio/static/css/4.ae2f7fee.chunk.css"
+  },
+  {
+    "revision": "1d6988683a7baf4822bd",
+    "url": "/Pavan-Portfolio/static/css/2.8dda707d.chunk.css"
+  },
+  {
+    "revision": "8e5ff67134b2be0aaf7d04079bb06812",
+    "url": "/Pavan-Portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.ea85679c64f329d96bd4a8b449b2ab49.js"
+  "/Pavan-Portfolio/precache-manifest.8574475239cd4a1c0ca577e023913eea.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/Pavan-Portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
