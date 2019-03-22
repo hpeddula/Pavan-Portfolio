d852b463e24da498c338b511010d81c3bf994c79
hpeddula <hpeddula@outlook.com>
2019-03-22 11:03:39 +0530

    Styled the not found component

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import './notfound.scss';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export default class NotFound extends Component {
     render() {
         return (
             <React.Fragment>
-                <div className="container-fluid not-found-container">
+                <div className="container-fluid">
                     <div className="not-found-box">
                         <h2>Oops! Wrong Page</h2>
-                        <div>
-                            <Link to="/">Back To Home</Link>
-                        </div>
+                    </div>
+                    <div className="not-found-button">
+                        <Link to="/">
+                            <FontAwesomeIcon icon={faHome} size="2x" /> Back To Home
+                        </Link>
                     </div>
                 </div>
             </React.Fragment>
